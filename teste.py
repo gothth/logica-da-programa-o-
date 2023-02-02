f78a2c07c283ff6488eec1cdd2d43efa2be56914
gothth <124213799+gothth@users.noreply.github.com>
2023-02-01 21:50:20 -0300

    Add files via upload

---
+++ teste.py
@@ -0,0 +1,18 @@
+print('testando o vscode')
+a=2
+b=3
+s=a+b
+print('soma=',s)
+print(type(s))
+print('testando o vscode')
+a=2.35
+b=3.15
+s=a+b
+print('soma=',s)
+print(type(s))
+print('testando o vscode')
+a='2'
+b='3'
+s=a+b
+print('soma=',s)
+print(type(s))
